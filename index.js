8ed27f4e2c3b68d27e21f91777600021e04e8ab0
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-08-11 11:39:56 -0600

    added to lab 2

--- index.js
+++ index.js
@@ -7,12 +7,16 @@
 const Input = require('./lib/input.js');
 // This communicates that you want require to look in this file path for the input library file.
 
+console.log('Input', Input);
+
 const notes = require('./lib/notes.js');
 // Requires to look in the filepath for the notes library file.
 
+console.log('notes', notes);
 
 
-const options = new Input();
-// creates new object from the Input constructor function
+// const options = 
+new Input();
+// // creates new object from the Input constructor function
 
-notes.fetch(options);
+// notes.fetch(options);
