4fab9595f608d234ab43d965eadf818893ca7c77
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-08-11 23:19:24 -0600

    attempting to make this work

--- index.js
+++ index.js
@@ -7,16 +7,17 @@
 const Input = require('./lib/input.js');
 // This communicates that you want require to look in this file path for the input library file.
 
-console.log('Input', Input);
+// console.log('Input', Input);
 
-const notes = require('./lib/notes.js');
+const Notes = require('./lib/notes.js');
 // Requires to look in the filepath for the notes library file.
 
-console.log('notes', notes);
+// console.log('notes', Notes);
 
 
 // const options = 
-new Input();
+const input = new Input();
+const note = new Notes(input);
 // // creates new object from the Input constructor function
 
-// notes.fetch(options);
+console.log(note);
